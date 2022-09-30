9cea9b0e7af3854ffdf3ecf0cab4e4e37e416f64
Asif saheer <asifsaheer70334@gmail.com>
2022-09-30 18:03:55 +0530

    products added

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.971a7412dce3e3b7ae913a69b531f07f.js"
+  "/precache-manifest.1ee829d4cc3ee3c67b9c032dc0a32cc4.js"
 );
 
 self.addEventListener('message', (event) => {
